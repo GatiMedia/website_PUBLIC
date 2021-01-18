6f9d4e0ec0bdebaae02f14cdbb90ab2014728327
Attila Gasparetz <45799912+GatiMedia@users.noreply.github.com>
2021-01-18 21:13:47 +0000

    Update oldTextFinder.py

--- Scripts/oldTextFinder.py
+++ Scripts/oldTextFinder.py
@@ -10,10 +10,20 @@ def oldTextFinder():
                     oldText.append(node.name())
                 except Exception:
                     pass
-        if nuke.ask(
-                "<b><center><font color=orange>These are the old Text nodes in your script:\n\n<font color=yellow>" + ', '.join(
-                        oldText) + "\n\n<font color=orange>Would you like to zoom on them?"):
+        for i in nuke.allNodes('Group'):
+            i.begin()
+            for node in nuke.allNodes('Text'):
+                try:
+                    node.setSelected(True)
+                    oldText.append(i.name() + " (is a Group node with Text inside)")
+                    nodeNames = [n.name() for n in nuke.selectedNodes()]
+                    if nodeNames:
+                        i.setSelected(True)
+                except Exception:
+                    pass
+            i.end()
+        if nuke.ask("<b><center><font color=orange>These are the old Text nodes in your script:\n\n<font color=yellow>" + ', '.join(oldText) + "\n\n<font color=orange>Would you like to zoom on them?"):
             nuke.zoomToFitSelected()
     else:
         nuke.message(
-            """<b><center><font color=orange>You don't have any old Text node\n on the root level.\n\n<a href="https://www.gatimedia.co.uk/oldtext2newtext"><font color=yellow><u>Learn about Old vs. New Text\n""")
+            """<b><center><font color=orange>You don't have any old Text node.\n\n<a href="https://www.gatimedia.co.uk/oldtext2newtext"><font color=yellow><u>Learn about Old vs. New Text\n""")
